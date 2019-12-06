5944e996c0f5b4a551676102e893105d098b0a9f
Noah Gibson <noah.gibson99@gmail.com>
2019-12-06 12:54:53 -0700

    Test fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,5 @@
 export const getProjects = async () => {
+  console.log('using the right url', 'https://pp-be.herokuapp.com/api/v1/projects')
   const response = await fetch('https://pp-be.herokuapp.com/api/v1/projects');
   if(!response.ok) {
     throw new Error('There was an error loading projects')
