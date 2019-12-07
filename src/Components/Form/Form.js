99bb1f12bad88a5e3f0418383a858f67952fc4c1
Zoe <zoekharvey@gmail.com>
2019-12-07 12:31:10 -0700

    Add palette and form component setup

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Form extends Component {
+  constructor() {
+    super()
+  }
+
+
+  render() {
+    return(
+      <h1>Form</h1>
+    )
+  }
+}
---
+++ src/Components/Palette/palette.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Form from '../Form'
+
+export class Palette extends Component {
+  constructor() {
+    super()
+  }
+
+
+
+
+  render() {
+    return(
+      <section className="palette-page">
+      <h1>palette page</h1>
+      <Form />
+      </section>
+    )
+  }
+}
