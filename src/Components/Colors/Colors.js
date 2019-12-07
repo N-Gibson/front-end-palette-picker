d7c2e9d5b12f183ed18c8c1d84349b34429472c1
Zoe <zoekharvey@gmail.com>
2019-12-07 16:02:00 -0700

    Add visuals to locked and unlocked states

--- src/Components/Colors/Colors.js
+++ src/Components/Colors/Colors.js
@@ -3,22 +3,30 @@ import '../Colors/Colors.scss';
 import lockIcon from '../../images/locked-icon.png'
 
 export const Colors = (props) => {
-  const {color1, color2, color3, color4, color5} = props;
+  const {color1, color2, color3, color4, color5, lockColor} = props;
+
+  const lockedStyle = {
+    opacity: 1
+  }
 
   return(
     <section className="colors-page">
-      <div className="color div__color--1" style={{backgroundColor: color1}}>
-        <img src={lockIcon}/></div>
-      <div className="color div__color--2" style={{backgroundColor: color2}} >
-      <img src={lockIcon}/>
+      <div className="color div__color--1" style={{backgroundColor: color1.hex}}>
+      {!color1.isLocked && <img src={lockIcon} onClick={() => lockColor( 'color1' )}/>}
+      {color1.isLocked  && <img style={lockedStyle} src={lockIcon} onClick={() => lockColor( 'color1' )}/>}
       </div>
-        <div className="color div__color--3" style={{backgroundColor: color3}}>
+      <div className="color div__color--2" style={{backgroundColor: color2.hex}} >
+      {!color2.isLocked && <img src={lockIcon} onClick={lockColor}/>}
+      {color2.isLocked  && <img style={lockedStyle} src={lockIcon} onClick={lockColor}/>}
+      
+      </div>
+        <div className="color div__color--3" style={{backgroundColor: color3.hex}}>
         <img src={lockIcon}/>
         </div>
-        <div className="color div__color--4" style={{backgroundColor: color4}}>
+        <div className="color div__color--4" style={{backgroundColor: color4.hex}}>
         <img src={lockIcon}/>
         </div>
-        <div className="color div__color5" style={{backgroundColor: color5}}>
+        <div className="color div__color5" style={{backgroundColor: color5.hex}}>
         <img src={lockIcon}/>
         </div>
     </section>
--- src/Components/Palette/palette.js
+++ src/Components/Palette/palette.js
@@ -10,17 +10,23 @@ export class Palette extends Component {
     super()
     this.state = {
       name: '',
-      color1: '#818479',
-      color2: '#B5CBB7',
-      color3: '#D2E4C4',
-      color4: '#E4E9B2',
-      color5: '#E7E08B'
+      color1: {hex: '#818479', isLocked: false},
+      color2: {hex: '#B5CBB7', isLocked: false},
+      color3: {hex: '#D2E4C4', isLocked: false},
+      color4: {hex: '#E4E9B2', isLocked: false},
+      color5: {hex: '#E7E08B', isLocked: false}
     }
   }
 
   generateColor = () => {
     let color1 = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
-    this.setState({ color1: color1})
+    // this.setState({ color1: color1})
+    this.setState(prevState => ({
+      color1: {                   
+          ...prevState.color1,    
+          hex: color1
+      }
+  }))
     let color2 = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     this.setState({ color2: color2})
     let color3 = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
@@ -31,7 +37,23 @@ export class Palette extends Component {
     this.setState({ color5: color5})
   }
 
-
+  lockColor = () => {
+    if(!this.state.color1.isLocked) {
+    this.setState(prevState => ({
+      color1: {                   
+          ...prevState.color1,    
+          isLocked: true
+      }
+  }))
+  } else {
+    this.setState(prevState => ({
+      color1: {                   
+          ...prevState.color1,    
+          isLocked: false
+      }
+  }))
+  }
+  }
   render() {
     return(
       <section className="palette-page">
@@ -39,6 +61,7 @@ export class Palette extends Component {
       <Nav generateColor = {this.generateColor}/>
      
       <Colors 
+        lockColor={this.lockColor}
         color1={this.state.color1}
         color2={this.state.color2}
         color3={this.state.color3}
