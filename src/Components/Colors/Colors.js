9b594f0080d4a987d679a483397d806a4809a68b
Zoe <zoekharvey@gmail.com>
2019-12-08 20:32:06 -0700

    Style hex codes

--- src/Components/Colors/Colors.js
+++ src/Components/Colors/Colors.js
@@ -14,24 +14,24 @@ export const Colors = (props) => {
       <div className="color div__color--1" style={{backgroundColor: color1.hex}}>
       {!color1.isLocked && <img src={lockIcon} onClick={() => lockColor( 'color1' )}/>}
       {color1.isLocked  && <img style={lockedStyle} src={lockIcon} onClick={() => lockColor( 'color1' )}/>}
-      {color1.hex}
+      <h2 className="color__hex">{color1.hex}</h2>
       </div>
       <div className="color div__color--2" style={{backgroundColor: color2.hex}} >
       {!color2.isLocked && <img src={lockIcon} onClick={lockColor}/>}
       {color2.isLocked  && <img style={lockedStyle} src={lockIcon} onClick={lockColor}/>}
-      {color2.hex}
+      <h2 className="color__hex">{color2.hex}</h2>
       </div>
         <div className="color div__color--3" style={{backgroundColor: color3.hex}}>
         <img src={lockIcon}/>
-        {color3.hex}
+        <h2 className="color__hex">{color3.hex}</h2>
         </div>
         <div className="color div__color--4" style={{backgroundColor: color4.hex}}>
         <img src={lockIcon}/>
-        {color4.hex}
+        <h2 className="color__hex">{color4.hex}</h2>
         </div>
         <div className="color div__color5" style={{backgroundColor: color5.hex}}>
         <img src={lockIcon}/>
-        {color4.hex}
+        <h2 className="color__hex">{color5.hex}</h2>
         </div>
     </section>
   )
