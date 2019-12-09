52da74e4667b322c8fa83b664970be9037b7480c
Noah Gibson <noah.gibson99@gmail.com>
2019-12-08 19:11:07 -0700

    Add link to project

--- src/Project/project.js
+++ src/Project/project.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { deleteProject } from '../apiCalls';
+import { Link } from 'react-router-dom';
 import './project.scss';
 
 const Project = (props) => {
   return (
+    <Link to={`/project/${props.id}`}>
     <section className='project' id={`project_${props.id}`}>
       <h2>{props.name}</h2>
       <button type='button' onClick={
@@ -12,6 +14,7 @@ const Project = (props) => {
         }
       }>Delete</button>
     </section>
+    </Link>
   )
 }
 
