5fe497823fa360530e095444f5063eb25452d6c4
Zoe <zoekharvey@gmail.com>
2019-12-08 20:41:47 -0700

    Add successful post for palettes

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -16,3 +16,31 @@ export const getPalettes = async () => {
   return data
 }
 
+export const postPalette = async (color1, color2, color3, color4, color5, projectId, paletteName) => {
+  const url = 'https://pp-be.herokuapp.com/api/v1/palettes/'
+  const body = {
+    color1: color1,
+    color2: color2,
+    color3: color3,
+    color4: color4,
+    color5: color5,
+    project_id: projectId,
+    name: paletteName
+  };
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body)
+  };
+
+  try {
+    const res = await fetch(url, options);
+    if (!res.ok) {
+      throw new Error('There was an error adding this palette');
+    }
+    const newPalette = await res.json();
+    return newPalette;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
