87c29ae6c4880b79a23e623556efb70816f073b6
Zoe <zoekharvey@gmail.com>
2019-12-10 14:53:31 -0700

    Append colors to all palette page

--- src/Components/AllPalettes/AllPalettes.js
+++ src/Components/AllPalettes/AllPalettes.js
@@ -3,9 +3,21 @@ import './AllPalettes.scss';
 
 const AllPalettes = (props) => {
   console.log('props in pallettes', props)
+  const palettes = props.palettes.map(palette => {
+    return(
+      <div>
+        <h3>{palette.name}</h3>
+        <div style={{backgroundColor: palette.color1}}>{palette.color1}</div>
+        <div style={{backgroundColor: palette.color2}}>{palette.color2}</div>
+        <div style={{backgroundColor: palette.color3}}>{palette.color3}</div>
+        <div style={{backgroundColor: palette.color4}}>{palette.color4}</div>
+        <div style={{backgroundColor: palette.color5}}>{palette.color5}</div>
+      </div>
+    )
+  })
   return (
     <div>
-      <h1>YOoOOOOOOO</h1>
+      {palettes}
     </div>
   )
 }
